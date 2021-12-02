d6b19cda3b97e7540dda9d4551e072ddc57e5ac9
Jakub Kądziołka <kuba@kadziolka.net>
2021-12-02 11:27:07 +0100

    Day 2
    If you can think of anything I could prove about this puzzle, let me
    know >.<

---
+++ README.md
@@ -0,0 +1,8 @@
+# aoc2021-isabelle
+
+Solutions to the Advent of Code 2021 puzzles, in Isabelle/HOL. At least,
+when I can think of a way to state correctness separately from the
+implementation.
+
+Since the programs extracted from Isabelle can't do I/O, each problem
+also gets a parser written in Haskell - find those in `io`.
---
+++ io/Day2.hs
@@ -0,0 +1,39 @@
+import Data.Functor
+
+type Position = (Integer, Integer)
+type Move = (String, Integer)
+
+type Position' = (Integer, Integer, Integer)
+
+go :: Move -> Position -> Position
+go ("forward", n) (x, y) = (x + n, y)
+go ("down", n) (x, y) = (x, y + n)
+go ("up", n) (x, y) = (x, y - n)
+
+go' :: Move -> Position' -> Position'
+go' ("forward", n) (x, y, v) = (x + n, y + n * v, v)
+go' ("down", n) (x, y, v) = (x, y, v + n)
+go' ("up", n) (x, y, v) = (x, y, v - n)
+
+parseLine :: String -> Move
+parseLine s =
+  let [dir, dist] = words s in
+  (dir, read dist)
+
+readInput :: String -> IO [Move]
+readInput fname = do
+  input <- readFile fname
+  return $ parseLine <$> lines input
+
+part1 :: [Move] -> Integer
+part1 = (uncurry (*)) . foldl (flip go) (0, 0)
+
+part2 :: [Move] -> Integer
+part2 = (\(x, y, v) -> x * y) . foldl (flip go') (0, 0, 0)
+
+main = do
+  readInput "input/day2-example.txt" <&> part1 >>= print
+  readInput "input/day2.txt" <&> part1 >>= print
+
+  readInput "input/day2-example.txt" <&> part2 >>= print
+  readInput "input/day2.txt" <&> part2 >>= print
